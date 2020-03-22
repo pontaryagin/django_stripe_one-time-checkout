510fc7c8db3e49eaff5dff3af912c0eac3729ed9
d0d0npa <kento.k.92@gmail.com>
2020-03-23 01:08:01 +0900

    Renamed function

--- checkout/urls.py
+++ checkout/urls.py
@@ -6,7 +6,7 @@ app_name = 'checkout'
 urlpatterns = [
     path('',views.IndexView.as_view(), name="index"),
     path('create-checkout-session/',views.onetime_payment_checkout , name='create-checkout'),
-    path('config/',views.stripe_config_tmp),
+    path('config/',views.stripe_config),
     path('success/',views.SuccessView.as_view() ),
     path('canceled/', views.CanceledView.as_view())
 ]
